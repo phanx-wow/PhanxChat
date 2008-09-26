7fcd576fcb1453f6c67f69032ff337821c3d6636
Phanx-28751 <Phanx-28751@06352937-199f-4489-bb52-8e4cf25e7443>
2008-09-26 10:57:25 +0000

    - Add ruRU translations from Valle of Warsong

---
+++ trunk/Locales/Locale-ruRU.lua
@@ -0,0 +1,91 @@
+﻿--[[
+	ruRU translations for PhanxChat
+	Contributed by Valle of Warsong
+]]
+
+if GetLocale() == "ruRU" then PHANXCHAT_LOCALS = {
+
+	CHANNEL_GENERAL			= "Общий",
+	CHANNEL_TRADE				= "Торговля",
+	CHANNEL_LOCALDEFENSE		= "Оборона: локальный",
+	CHANNEL_WORLDDEFENSE		= "Оборона: глобальный",
+	CHANNEL_LOOKINGFORGROUP		= "Поис спутников",
+	CHANNEL_GUILDRECRUITMENT		= "Набор в гильдии",
+
+	SHORT_GENERAL 				= "О",
+	SHORT_TRADE 				= "Т",
+	SHORT_LOCALDEFENSE 			= "ЛО",
+	SHORT_WORLDDEFENSE 			= "МО",
+	SHORT_LOOKINGFORGROUP		= "ЛФГ",
+	SHORT_GUILDRECRUITMENT		= "НвГ",
+
+	SHORT_SAY 				= "С",
+	SHORT_YELL 				= "К",
+	SHORT_GUILD 				= "Г",
+	SHORT_OFFICER 				= "Оф",
+	SHORT_PARTY 				= "Гр",
+	SHORT_RAID 				= "Р",
+	SHORT_RAID_LEADER 			= "РЛ",
+	SHORT_RAID_WARNING 			= "РВ",
+	SHORT_BATTLEGROUND 			= "ПБ",
+	SHORT_BATTLEGROUND_LEADER 	= "ЛПБ",
+	SHORT_WHISPER 				= "Ш",
+	SHORT_WHISPER_INFORM 		= "Ш",
+
+	["enabled"] = "Включено",
+	["disabled"] = "Выключено",
+
+	["Use /pchat with the following commands:"] = "Используйте /pchat со следующими командами:",
+
+	["Hide the scroll buttons"] = "Скрыть кнопки чата",
+	["Button hiding %s."] = "Скрытие кнопок %s.",
+
+	["Shorten channel names and chat strings"] = "Сокращать названия каналов и строки чата",
+	["Short channel names %s."] = "Короткие названия каналов %s.",
+
+	["Clear the window"] = "Очистить окно",
+
+	["Enable arrow keys in the edit box"] = "Использовать кнопки передвижения в строке ввода",
+	["Edit box arrow keys %s."] = "Кнопки передвижения в строке ввода %s.",
+
+	["Move the edit box above the window"] = "Переместить строку ввода наверх",
+	["Edit box relocation %s."] = "Перемещение строки ввода %s.",
+
+	["Set the text fade time in minutes (0 = disabled)"] = "Установить время исчезновения текста в минутах (0 = отключить)",
+	["Chat fading %s."] = "Исчезновение текста %s.",
+	["Chat fade time set to %d minutes."] = "Время исчезновения текста установлено на %d минут.",
+	["Fade time is currently set to %d minutes."] = "Время исчезновения текста в данный момент установлено на %d минут.",
+
+	["Stop tabs from flashing"] = "Отключить мерцание закладок",
+	["Chat tab flash suppression %s."] = "Отключение мерцания закладок  %s",
+
+	["Set the font size for all windows"] = "Установить размер шрифта для всех окон",
+	["Chat font size set to %d."] = "Установлен %d размер шрифта.",
+
+	["Start chat logging automatically"] = "Автоматическое включение записи чата",
+	["Automatic chat logging %s."] = "Автоматическое включение записи чата %s.",
+
+	["Color player names by class if known"] = "Окрашивать имена игроков в цвет класса если класс известен",
+	["Player name class coloring %s."] = "Окраска  имен игроков %s.",
+
+	["Enable mousewheel scrolling"] = "Включить прокрутку колесиком мыши",
+	["Mousewheel scrolling %s."] = "Прокрутка колесиком мыши %s.",
+
+	["Make more chat types sticky"] = "'Липкие каналы'",
+	["Sticky channels %s."] = "'Липкие' каналы %s.",
+
+	["Suppress channel notification messages"] = "Подавление уведомлений каналов",
+	["Channel notice suppression %s."] = "Подавление уведомлений %s.",
+
+	["Suppress repeated messages in public channels"] = "Подавление повторяющихся сообщений в общих каналах",
+	["Repeated message suppression %s."] = "Подавление повторяющихся сообщений %s.",
+
+	["Lock docked tabs in place (hold Alt to drag)"] = "Закрепить закладки чата (удерживайте Alt для перемещения)",
+	["Chat tab locking %s."] = "Закрепление закладок чата %s.",
+
+	["Link URLs for easy copying"] = "Кликабельные URL для облегчения копирования",
+	["URL linking %s."] = "Кликабельные URL %s.",
+
+	["Version %d loaded."] = "Загружена версия %d.",
+
+} end
