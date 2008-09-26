f2ebc905dddfb9ca9a0f71f6d110c554f7631e4f
Phanx-28751 <Phanx-28751@06352937-199f-4489-bb52-8e4cf25e7443>
2008-09-26 23:53:49 +0000

    - Add deDE translations from Melikae

---
+++ trunk/Locales/Locale-deDE.lua
@@ -0,0 +1,93 @@
+--[[
+	deDE translations for PhanxChat
+	Contributed by Melikae - Proudmoore (melikae@gmx.eu)
+]]
+
+if GetLocale() == "deDE" then PHANXCHAT_LOCALS = {
+
+	CHANNEL_GENERAL			= "Allgemein",
+	CHANNEL_TRADE				= "Handel",
+	CHANNEL_LOCALDEFENSE		= "LokaleVerteidigung",
+	CHANNEL_WORLDDEFENSE		= "GlobaleVerteidigung",
+	CHANNEL_LOOKINGFORGROUP		= "SucheNachGruppe",
+	CHANNEL_GUILDRECRUITMENT		= "Gildenrekrutierung",
+
+	WHO_QUERY_RESULT			= "^%|Hplayer:%w+|h%[(%w+)%]|h: Level %d+ %w+%s?[%w]* (%w+)%s?<?[^>]*>? %- .+$",
+
+	SHORT_GENERAL 				= "A",
+	SHORT_TRADE 				= "H",
+	SHORT_LOCALDEFENSE 			= "LV",
+	SHORT_WORLDDEFENSE 			= "GV",
+	SHORT_LOOKINGFORGROUP		= "LFG",
+	SHORT_GUILDRECRUITMENT		= "R",
+
+	SHORT_SAY 				= "S",
+	SHORT_YELL 				= "S",
+	SHORT_GUILD 				= "G",
+	SHORT_OFFICER 				= "O",
+	SHORT_PARTY 				= "G",
+	SHORT_RAID 				= "SZ",
+	SHORT_RAID_LEADER 			= "SZL",
+	SHORT_RAID_WARNING 			= "SZW",
+	SHORT_BATTLEGROUND 			= "SF",
+	SHORT_BATTLEGROUND_LEADER 	= "SFL",
+	SHORT_WHISPER 				= "W",
+	SHORT_WHISPER_INFORM 		= "W",
+
+	["enabled"] = "aktiviert",
+	["disabled"] = "deaktiviert",
+
+	["Use /pchat with the following commands:"] = "Benutze /pchat mit folgenden Anweisung:",
+
+	["Hide the scroll buttons"] = "Scroll Buttons verstecken",
+	["Button hiding %s."] = "Buttons verstecken %s.",
+
+	["Shorten channel names and chat strings"] = "Kanal Namen und Chat Bezeichnungen abkürzen",
+	["Short channel names %s."] = "Kurze Kanal Namen %s.",
+
+	["Clear the window"] = "Fenster leeren",
+
+	["Enable arrow keys in the edit box"] = "Pfeiltasten für das Eingabefeld aktivieren",
+	["Edit box arrow keys %s."] = "Pfeiltasten für das Eingabefeld  %s.",
+
+	["Move the edit box above the window"] = "Eingabefeld über dem Chatfenster anzeigen",
+	["Edit box relocation %s."] = "Verschieben der Eingabezeile %s.",
+
+	["Set the text fade time in minutes (0 = disabled)"] = "Zeit bis zum Ausblenden des Textes in Minuten (0 = deaktiviert)",
+	["Chat fading %s."] = "Ausblenden des Textes %s.",
+	["Chat fade time set to %d minutes."] = "Zeit bis zum Ausblenden des Textes auf %d Minuten gesetzt.",
+	["Fade time is currently set to %d minutes."] = "Zeit bis zum Ausblenden des Textes ist im Moment auf %d Minuten gesetzt.",
+
+	["Stop tabs from flashing"] = "Blinken der Tabs verhindern",
+	["Chat tab flash suppression %s."] = "Blinken der Tabs verhindern %s",
+
+	["Set the font size for all windows"] = "Schriftgröße für alle Fenster einstellen",
+	["Chat font size set to %d."] = "Schriftgröße eingestellt auf %d.",
+
+	["Start chat logging automatically"] = "Chat automatisch aufzeichen",
+	["Automatic chat logging %s."] = "Automatisches Aufzeichnen des Chats %s.",
+
+	["Color player names by class if known"] = "Namen der Spieler nach Klasse einfärben, wenn bekannt",
+	["Player name class coloring %s."] = "Einfärben der Spielernamen nach Klasse %s.",
+
+	["Enable mousewheel scrolling"] = "Mit dem Mausrad scrollen aktivieren",
+	["Mousewheel scrolling %s."] = "Mausrad scrollen %s.",
+
+	["Make more chat types sticky"] = "Mehr Kanäle \"sticky\" machen",
+	["Sticky channels %s."] = "Kanäle merken %s.",
+
+	["Suppress channel notification messages"] = "Kanal Meldungen unterdrücken",
+	["Channel notice suppression %s."] = "Unterdrücken der Kanal Meldungen %s.",
+
+	["Suppress repeated messages in public channels"] = "Wiederholungen in öffentlichen Channels unterdrücken",
+	["Repeated message suppression %s."] = "Wiederholungen unterdrücken %s.",
+
+	["Lock docked tabs in place (hold Alt to drag)"] = "Verschieben angedockter Tabs verhindern (zum Verschieben Alt gedrückt halten",
+	["Chat tab locking %s."] = "Verschieben von Tabs verhindern %s.",
+
+	["Link URLs for easy copying"] = "URLs für einfaches kopieren anklickbar machen",
+	["URL linking %s."] = "URLs anklickbar %s.",
+
+	["Version %d loaded."] = "Version %d geladen.",
+
+} end
