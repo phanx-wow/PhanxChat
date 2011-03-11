6a544ef5099651ef798d65d2a7987e487c4de1d0
Akkorian-257529 <Akkorian-257529@06352937-199f-4489-bb52-8e4cf25e7443>
2011-03-11 05:20:59 +0000

    Improved the appearance of highlighted and selected tabs Updated the TOC for WoW 4.1

--- trunk/Modules/HideTextures.lua
+++ trunk/Modules/HideTextures.lua
@@ -9,30 +9,117 @@
 ----------------------------------------------------------------------]]
 
 local _, PhanxChat = ...
-
+local hooks = PhanxChat.hooks
 local noop = function() end
 
+local SELECTED_TAB_COLOR = { r = 1, g = 1, b = 1 }
+
+local function Tab_OnEnter(tab)
+	hooks[tab].OnEnter(tab)
+	if tab.frame.selected then return end
+	tab.text:SetTextColor(1, 1, 1)
+end
+
+local function Tab_OnLeave(tab)
+	hooks[tab].OnLeave(tab)
+	if tab.frame.selected then return end
+	tab.text:SetTextColor(1, 0.8, 0)
+end
+
+local function Tab_OnClick(tab, button)
+	hooks.FCF_Tab_OnClick(tab, button)
+
+	local selected = SELECTED_CHAT_FRAME
+	for frame in pairs(PhanxChat.frames) do
+		frame.selected = frame == selected
+		if frame.selected then
+			frame.tab.text:SetTextColor(1, 1, 1)
+			frame.tab.fullHighlightTexture:SetAlpha(0)
+		else
+			frame.tab.text:SetTextColor(1, 0.8, 0)
+			frame.tab.fullHighlightTexture:SetAlpha(1)
+		end
+	end
+end
+
+local function Tab_UpdateColors(tab, selected)
+	hooks.FCFTab_UpdateColors(tab, selected)
+
+	local selected = SELECTED_CHAT_FRAME
+	for frame in pairs(PhanxChat.frames) do
+		frame.selected = frame == selected
+		if frame.selected then
+			frame.tab.text:SetTextColor(1, 1, 1)
+			frame.tab.fullHighlightTexture:SetAlpha(0)
+		else
+			frame.tab.text:SetTextColor(1, 0.8, 0)
+			frame.tab.fullHighlightTexture:SetAlpha(1)
+		end
+	end
+end
+
 function PhanxChat:HideTextures(frame)
-	if self.db.HideTextures then
+	local selected = frame == SELECTED_CHAT_FRAME
+	frame.selected = selected
+
+	if not frame.tab then
 		local name = frame:GetName()
 
+		local tab =  _G[name .. "Tab"]
+		tab.frame = frame
+		tab.left = _G[name .. "TabLeft"]
+		tab.right = _G[name .. "TabRight"]
+		tab.mid = _G[name .. "TabMiddle"]
+		tab.text = _G[name .. "TabText"]
+		frame.tab = tab
+
+		local editBox = frame.editBox
+		editBox.left = _G[name .. "EditBoxLeft"]
+		editBox.right = _G[name .. "EditBoxRight"]
+		editBox.mid = _G[name .. "EditBoxMid"]
+
+		local highlight = tab:CreateTexture(nil, "HIGHLIGHT")
+		highlight:SetTexture([[Interface\PaperDollInfoFrame\UI-Character-Tab-Highlight]])
+		highlight:SetBlendMode("ADD")
+		highlight:SetPoint("LEFT", 0, -7)
+		highlight:SetPoint("RIGHT", 0, -7)
+		tab.fullHighlightTexture = highlight
+
+		hooks[tab] = { }
+	end
+
+	if self.db.HideTextures then
 		frame.clickAnywhereButton:SetScript("OnShow", frame.clickAnywhereButton.Hide)
 		frame.clickAnywhereButton:Hide()
 
-		local editBox = _G[name .. "EditBox"]
+		local editBox = frame.editBox
 
 		if editBox then
-			_G[name .. "EditBoxLeft"]:SetAlpha(0)
-			_G[name .. "EditBoxRight"]:SetAlpha(0)
-			_G[name .. "EditBoxMid"]:SetAlpha(0)
+			editBox.left:SetAlpha(0)
+			editBox.right:SetAlpha(0)
+			editBox.mid:SetAlpha(0)
 
 			editBox.focusLeft:SetTexture([[Interface\ChatFrame\UI-ChatInputBorder-Left2]])
 			editBox.focusRight:SetTexture([[Interface\ChatFrame\UI-ChatInputBorder-Right2]])
 			editBox.focusMid:SetTexture([[Interface\ChatFrame\UI-ChatInputBorder-Mid2]])
 		end
 
-		local tab = _G[name .. "Tab"]
-		local tabText = _G[name .. "TabText"]
+		local tab = frame.tab
+
+		tab.noMouseAlpha = 0
+		FCFTab_UpdateAlpha(frame)
+
+		tab.leftSelectedTexture:SetAlpha(0)
+		tab.middleSelectedTexture:SetAlpha(0)
+		tab.rightSelectedTexture:SetAlpha(0)
+
+		tab.leftHighlightTexture:SetAlpha(0)
+		tab.middleHighlightTexture:SetAlpha(0)
+		tab.rightHighlightTexture:SetAlpha(0)
+
+		tab.fullHighlightTexture:SetAlpha(selected and 0 or 1)
+
+		local tabText = tab.text
 
 		tabText:ClearAllPoints()
 		tabText:SetPoint("BOTTOMLEFT", tab, 16, 5)
@@ -40,76 +127,85 @@ function PhanxChat:HideTextures(frame)
 		tabText:SetJustifyH("LEFT")
 		tabText.GetWidth = tabText.GetStringWidth
 
-		tab.noMouseAlpha = 0
-		FCFTab_UpdateAlpha(frame)
-
-		tab.leftSelectedTexture:SetAlpha(0)
-		tab.leftHighlightTexture:SetTexture("")
-
-		tab.rightSelectedTexture:SetAlpha(0)
-		tab.rightHighlightTexture:SetTexture("")
-
-		tab.middleSelectedTexture:SetAlpha(0)
-		tab.middleHighlightTexture:SetTexture([[Interface\PaperDollInfoFrame\UI-Character-Tab-Highlight]])
-		tab.middleHighlightTexture:SetHorizTile(false)
-		tab.middleHighlightTexture:ClearAllPoints()
-		tab.middleHighlightTexture:SetHeight(32)
-		tab.middleHighlightTexture:SetPoint("LEFT", _G[name .. "TabLeft"], 0, -7)
-		tab.middleHighlightTexture:SetPoint("RIGHT", _G[name .. "TabRight"], 0, -7)
-
-		if not self.hooks[tab.middleHighlightTexture] then
-			self.hooks[tab.middleHighlightTexture] = { }
+		if selected then
+			tabText:SetTextColor(1, 1, 1)
+		else
+			tabText:SetTextColor(1, 0.8, 1)
 		end
-		if not self.hooks[tab.middleHighlightTexture].SetVertexColor then
-			self.hooks[tab.middleHighlightTexture].SetVertexColor = tab.middleHighlightTexture.SetVertexColor
-			tab.middleHighlightTexture.SetVertexColor = noop
+
+		if not hooks.FCF_Tab_OnClick then
+			hooks.FCF_Tab_OnClick = FCF_Tab_OnClick
+			FCF_Tab_OnClick = Tab_OnClick
+		end
+		if not hooks.FCFTab_UpdateColors then
+			hooks.FCFTab_UpdateColors = FCFTab_UpdateColors
+			FCFTab_UpdateColors = Tab_UpdateColors
+		end
+		if not hooks[tab].OnEnter then
+			hooks[tab].OnEnter = tab:GetScript("OnEnter") or noop
+			tab:SetScript("OnLeave", Tab_OnEnter)
+		end
+		if not hooks[tab].OnLeave then
+			hooks[tab].OnLeave = tab:GetScript("OnLeave") or noop
+			tab:SetScript("OnLeave", Tab_OnLeave)
 		end
 	else
-		local name = frame:GetName()
-
 		frame.clickAnywhereButton:SetScript("OnShow", nil)
 		frame.clickAnywhereButton:Show()
 
-		local editBox = _G[name .. "EditBox"]
+		local editBox = frame.editBox
 
 		if editBox then
-			_G[name .. "EditBoxLeft"]:SetAlpha(1)
-			_G[name .. "EditBoxRight"]:SetAlpha(1)
-			_G[name .. "EditBoxMid"]:SetAlpha(1)
+			editBox.left:SetAlpha(1)
+			editBox.right:SetAlpha(1)
+			editBox.mid:SetAlpha(1)
 
 			editBox.focusLeft:SetTexture([[Interface\ChatFrame\UI-ChatInputBorderFocus-Left]])
 			editBox.focusRight:SetTexture([[Interface\ChatFrame\UI-ChatInputBorderFocus-Right]])
 			editBox.focusMid:SetTexture([[Interface\ChatFrame\UI-ChatInputBorderFocus-Mid]])
 		end
 
-		local tab = _G[name .. "Tab"]
-		local tabText = _G[name .. "TabText"]
+		local tab = frame.tab
 
 		tab.noMouseAlpha = 0.4
 		FCFTab_UpdateAlpha(frame)
 
+		tab.leftSelectedTexture:SetAlpha(1)
+		tab.rightSelectedTexture:SetAlpha(1)
+		tab.middleSelectedTexture:SetAlpha(1)
+
+		tab.leftHighlightTexture:SetAlpha(1)
+		tab.rightHighlightTexture:SetAlpha(1)
+		tab.middleHighlightTexture:SetAlpha(1)
+
+		tab.fullHighlightTexture:SetAlpha(0)
+
+		local tabText = tab.text
+
 		tabText:ClearAllPoints()
-		tabText:SetPoint("LEFT", _G[name .. "TabRight"], "RIGHT", 0, -5)
+		tabText:SetPoint("LEFT", tab.right, "RIGHT", 0, -5)
 		tabText.GetWidth = nil
 
-		tab.leftSelectedTexture:SetAlpha(1)
-		tab.leftHighlightTexture:SetTexture([[Interface\ChatFrame\ChatFrameTab-HighlightLeft]])
+		tabText:SetTextColor(1, 0.8, 0)
 
-		tab.rightSelectedTexture:SetAlpha(1)
-		tab.rightHighlightTexture:SetTexture([[Interface\ChatFrame\ChatFrameTab-HighlightRight]])
-
-		tab.middleSelectedTexture:SetAlpha(1)
-		tab.middleHighlightTexture:SetTexture([[Interface\ChatFrame\ChatFrameTab-HighlightMid]])
-		tab.middleHighlightTexture:SetHorizTile(true)
-		tab.middleHighlightTexture:ClearAllPoints()
-		tab.middleHighlightTexture:SetWidth(44)
-		tab.middleHighlightTexture:SetHeight(32)
-		tab.middleHighlightTexture:SetPoint("TOPLEFT", _G[name .. "TabMiddle"], 0, 0)
-		tab.middleHighlightTexture:SetPoint("TOPRIGHT", _G[name .. "TabMiddle"], 0, 0)
-
-		if self.hooks[tab.middleHighlightTexture] and self.hooks[tab.middleHighlightTexture].SetVertexColor then
-			tab.middleHighlightTexture.SetVertexColor = self.hooks[tab.middleHighlightTexture].SetVertexColor
-			self.hooks[tab.middleHighlightTexture].SetVertexColor = nil
+		if hooks.FCF_Tab_OnClick then
+			FCF_Tab_OnClick = hooks.FCF_Tab_OnClick
+			hooks.FCF_Tab_OnClick = nil
+		end
+		if hooks.FCFTab_UpdateColors then
+			FCFTab_UpdateColors = hooks.FCFTab_UpdateColors
+			hooks.FCFTab_UpdateColors = nil
+			FCFTab_UpdateColors(tab, selected)
+		end
+		local enter = hooks[tab].OnEnter
+		if enter then
+			tab:SetScript("OnEnter", enter ~= noop and enter or nil)
+			hooks[tab].OnEnter = nil
+		end
+		local leave = hooks[tab].OnLeave
+		if leave then
+			tab:SetScript("OnLeave", leave ~= noop and leave or nil)
+			hooks[tab].OnLeave = nil
 		end
 	end
 end
