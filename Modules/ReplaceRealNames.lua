d4e1fc47c258e6fdb80ea31e3a2f7017c0ce94cc
Phanx <addons@phanx.net>
2011-12-11 10:20:49 +0000

    - Update author info

--- Modules/ReplaceRealNames.lua
+++ Modules/ReplaceRealNames.lua
@@ -2,7 +2,6 @@
 	PhanxChat
 	Reduces chat frame clutter and enhances chat frame functionality.
 	Written by Phanx <addons@phanx.net>
-	Maintained by Akkorian <akkorian@hotmail.com>
 	Copyright © 2006–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info6323-PhanxChat.html
 	http://wow.curse.com/downloads/wow-addons/details/phanxchat.aspx
