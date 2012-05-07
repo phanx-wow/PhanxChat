0b7d7c70d69471424285e17500d271501830c925
Phanx <addons@phanx.net>
2012-05-07 23:33:10 +0000

    - Add global "Show class colors" option

--- Options.lua
+++ Options.lua
@@ -24,7 +24,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local ShortenChannelNames = self:CreateCheckbox(L["Short channel names"])
 	ShortenChannelNames.desc = L["Shorten channel names and chat strings."]
 	ShortenChannelNames:SetPoint("TOPLEFT", notes, "BOTTOMLEFT", -2, -12)
-	ShortenChannelNames:SetChecked(db.ShortenChannelNames)
 	ShortenChannelNames.OnClick = function(self, checked)
 		if PhanxChat.debug then print("PhanxChat: ShortenChannelNames", checked) end
 		PhanxChat:SetShortenChannelNames(checked)
@@ -35,7 +34,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local ShortenPlayerNames = self:CreateCheckbox(L["Short player names"])
 	ShortenPlayerNames.desc = L["Shorten player names by removing realm names and Real ID last names."]
 	ShortenPlayerNames:SetPoint("TOPLEFT", ShortenChannelNames, "BOTTOMLEFT", 0, -8)
-	ShortenPlayerNames:SetChecked(db.ShortenPlayerNames)
 	ShortenPlayerNames.OnClick = function(self, checked)
 		if PhanxChat.debug then print("PhanxChat: ShortenPlayerNames", checked) end
 		db.ShortenPlayerNames = checked
@@ -46,7 +44,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local ReplaceRealNames = self:CreateCheckbox(L["Replace real names"])
 	ReplaceRealNames.desc = L["Replace Real ID names with character names."]
 	ReplaceRealNames:SetPoint("TOPLEFT", ShortenPlayerNames, "BOTTOMLEFT", 0, -8)
-	ReplaceRealNames:SetChecked(db.ReplaceRealNames)
 	ReplaceRealNames.OnClick = function(self, checked)
 		if PhanxChat.debug then print("PhanxChat: ReplaceRealNames", checked) end
 		PhanxChat:SetReplaceRealNames(checked)
@@ -57,7 +54,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local EnableArrows = self:CreateCheckbox(L["Enable arrow keys"])
 	EnableArrows.desc = L["Enable arrow keys in the chat edit box."]
 	EnableArrows:SetPoint("TOPLEFT", ReplaceRealNames, "BOTTOMLEFT", 0, -8)
-	EnableArrows:SetChecked(db.EnableArrows)
 	EnableArrows.OnClick = function(self, checked)
 		PhanxChat:SetEnableArrows(checked)
 	end
@@ -67,7 +63,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local EnableResizeEdges = self:CreateCheckbox(L["Enable resize edges"])
 	EnableResizeEdges.desc = L["Enable resize controls at all edges of chat frames, instead of only the bottom right corner."]
 	EnableResizeEdges:SetPoint("TOPLEFT", EnableArrows, "BOTTOMLEFT", 0, -8)
-	EnableResizeEdges:SetChecked(db.EnableResizeEdges)
 	EnableResizeEdges.OnClick = function(self, checked)
 		PhanxChat:SetEnableResizeEdges(checked)
 	end
@@ -77,7 +72,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local LinkURLs = self:CreateCheckbox(L["Link URLs"])
 	LinkURLs.desc = L["Transform URLs in chat into clickable links for easy copying."]
 	LinkURLs:SetPoint("TOPLEFT", EnableResizeEdges, "BOTTOMLEFT", 0, -8)
-	LinkURLs:SetChecked(db.LinkURLs)
 	LinkURLs.OnClick = function(self, checked)
 		PhanxChat:SetLinkURLs(checked)
 	end
@@ -87,7 +81,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local LockTabs = self:CreateCheckbox(L["Lock docked tabs"])
 	LockTabs.desc = L["Prevent docked chat tabs from being dragged unless the Shift key is down."]
 	LockTabs:SetPoint("TOPLEFT", LinkURLs, "BOTTOMLEFT", 0, -8)
-	LockTabs:SetChecked(db.LockTabs)
 	LockTabs.OnClick = function(self, checked)
 		PhanxChat:SetLockTabs(checked)
 	end
@@ -97,7 +90,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local MoveEditBox = self:CreateCheckbox(L["Move edit boxes"])
 	MoveEditBox.desc = L["Move chat edit boxes to the top their respective chat frame."]
 	MoveEditBox:SetPoint("TOPLEFT", LockTabs, "BOTTOMLEFT", 0, -8)
-	MoveEditBox:SetChecked(db.MoveEditBox)
 	MoveEditBox.OnClick = function(self, checked)
 		PhanxChat:SetMoveEditBox(checked)
 	end
@@ -107,7 +99,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local HideNotices = self:CreateCheckbox(L["Hide notices"])
 	HideNotices.desc = L["Hide channel notification messages."]
 	HideNotices:SetPoint("TOPLEFT", MoveEditBox, "BOTTOMLEFT", 0, -8)
-	HideNotices:SetChecked(db.HideNotices)
 	HideNotices.OnClick = function(self, checked)
 		PhanxChat:SetHideNotices(checked)
 	end
@@ -117,7 +108,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local HideRepeats = self:CreateCheckbox(L["Hide repeats"])
 	HideRepeats.desc = L["Hide repeated messages in public channels."]
 	HideRepeats:SetPoint("TOPLEFT", HideNotices, "BOTTOMLEFT", 0, -8)
-	HideRepeats:SetChecked(db.HideRepeats)
 	HideRepeats.OnClick = function(self, checked)
 		PhanxChat:SetHideRepeats(checked)
 	end
@@ -128,7 +118,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local HideButtons = self:CreateCheckbox(L["Hide buttons"])
 	HideButtons.desc = L["Hide the chat frame menu and scroll buttons."]
 	HideButtons:SetPoint("TOPLEFT", notes, "BOTTOM", 2, -12)
-	HideButtons:SetChecked(db.HideButtons)
 	HideButtons.OnClick = function(self, checked)
 		PhanxChat:SetHideButtons(checked)
 	end
@@ -138,7 +127,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local HideTextures = self:CreateCheckbox(L["Hide extra textures"])
 	HideTextures.desc = L["Hide the extra textures on chat tabs and chat edit boxes added in patch 3.3.5."]
 	HideTextures:SetPoint("TOPLEFT", HideButtons, "BOTTOMLEFT", 0, -8)
-	HideTextures:SetChecked(db.HideTextures)
 	HideTextures.OnClick = function(self, checked)
 		PhanxChat:SetHideTextures(checked)
 	end
@@ -148,7 +136,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	local HideFlash = self:CreateCheckbox(L["Hide tab flash"])
 	HideFlash.desc = L["Disable the flashing effect on chat tabs that receive new messages."]
 	HideFlash:SetPoint("TOPLEFT", HideTextures, "BOTTOMLEFT", 0, -8)
-	HideFlash:SetChecked(db.HideFlash)
 	HideFlash.OnClick = function(self, checked)
 		PhanxChat:SetHideFlash(checked)
 	end
@@ -194,7 +181,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	EnableSticky.desc = L["Set which chat types should be sticky."]
 	EnableSticky:SetPoint("TOPLEFT", HideFlash, "BOTTOMLEFT", 0, -14)
 	EnableSticky:SetPoint("TOPRIGHT", notes, "BOTTOMRIGHT", -2, -18 - ((HideFlash:GetHeight() + 8) * 3))
-	EnableSticky:SetValue(db.EnableSticky, stickyValues[db.EnableSticky])
 
 	--------------------------------------------------------------------
 
@@ -202,7 +188,6 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	FadeTime.desc = L["Set the time, in minutes, to wait before fading chat text. A setting of 0 will disable fading."]
 	FadeTime:SetPoint("TOPLEFT", EnableSticky, "BOTTOMLEFT", 0, -12)
 	FadeTime:SetPoint("TOPRIGHT", EnableSticky, "BOTTOMRIGHT", 0, -12)
-	FadeTime:SetValue(db.FadeTime)
 	FadeTime.OnValueChanged = function(self, value)
 		value = math.floor(value + 0.5)
 		PhanxChat:SetFadeTime(value)
@@ -215,8 +200,8 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 	FontSize.desc = L["Set the font size for all chat frames."] .. "\n\n" .. L["Note that this is just a shortcut to configuring each chat frame individually through the Blizzard chat options."]
 	FontSize:SetPoint("TOPLEFT", FadeTime, "BOTTOMLEFT", 0, -10)
 	FontSize:SetPoint("TOPRIGHT", FadeTime, "BOTTOMRIGHT", 0, -10)
-	FontSize:SetValue(math.floor(select(2, ChatFrame1:GetFont()) + 0.5))
 	FontSize.OnValueChanged = function(self, value)
+		db.FontSize = value
 		for frame in pairs(PhanxChat.frames) do
 			FCF_SetChatWindowFontSize(nil, frame, value)
 		end
@@ -224,7 +209,41 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 
 	--------------------------------------------------------------------
 
-	self.refresh = function( self )
+	local ShowClassColors = self:CreateCheckbox(L["Show class colors"])
+	ShowClassColors.desc = L["Show class colors in all channels."] .. "\n\n" .. L["Note that this is just a shortcut to configuring each chat channel individually through the Blizzard chat options."]
+	ShowClassColors:SetPoint("TOPLEFT", FontSize, "BOTTOMLEFT", 0, -8)
+	ShowClassColors.OnClick = function(self, checked)
+		db.ShowClassColors = checked
+		if checked then
+			for i = 1, 18 do
+				local check = _G["ChatConfigChatSettingsLeftCheckBox"..i.."Check"]
+				if check then
+					check:SetChecked(true)
+					check:Disable()
+					ToggleChatColorNamesByClassGroup(true, check:GetParent().type)
+				end
+			end
+			for i = 1, 50 do
+				ToggleChatColorNamesByClassGroup(true, "CHANNEL"..i)
+			end
+		else
+			for i = 1, 18 do
+				local check = _G["ChatConfigChatSettingsLeftCheckBox"..i.."Check"]
+				if check then
+					check:SetChecked(false)
+					check:Enable()
+					ToggleChatColorNamesByClassGroup(false, check:GetParent().type)
+				end
+			end
+			for i = 1, 50 do
+				ToggleChatColorNamesByClassGroup(false, "CHANNEL"..i)
+			end
+		end
+	end
+
+	--------------------------------------------------------------------
+
+	self.refresh = function(self)
 		HideFlash:SetChecked(db.HideFlash)
 		EnableArrows:SetChecked(db.EnableArrows)
 		EnableResizeEdges:SetChecked(db.EnableResizeEdges)
@@ -237,10 +256,13 @@ local panel = LibStub( "PhanxConfig-OptionsPanel" ).CreateOptionsPanel( PHANXCHA
 		ShortenPlayerNames:SetChecked(db.ShortenPlayerNames)
 		EnableSticky:SetValue(db.EnableSticky, stickyValues[db.EnableSticky])
 		FadeTime:SetValue(db.FadeTime)
-		FontSize:SetValue(math.floor(select(2, ChatFrame1:GetFont()) + 0.5))
+		FontSize:SetValue(db.FontSize or math.floor(select(2, ChatFrame1:GetFont()) + 0.5))
 		HideNotices:SetChecked(db.HideNotices)
 		HideRepeats:SetChecked(db.HideRepeats)
+		ShowClassColors:SetChecked(db.ShowClassColors)
 	end
+
+	self:refresh()
 end )
 
 LibStub("LibAboutPanel").new(PHANXCHAT, PHANXCHAT)
