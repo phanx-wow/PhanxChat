d507b8ea46a5c32bcf84f5690a0c9d5b9a65c2a2
Phanx-28751 <Phanx-28751@06352937-199f-4489-bb52-8e4cf25e7443>
2013-12-05 03:34:21 +0000

    - Add workaround for Blizzard breaking up/down without Alt keys to cycle through input history. Skips secure slash commands, since they cannot be re-added via SetText without causing an "action blocked" error.

--- trunk/Modules/EnableArrows.lua
+++ trunk/Modules/EnableArrows.lua
@@ -9,10 +9,63 @@
 
 local _, PhanxChat = ...
 
+local history = {}
+local index = {}
+
+local function AddHistoryLine(frame, text)
+	if not text or strlen(text) == 0 then
+		return
+	end
+	local command = strmatch(text, "^(/%S+)")
+	if command and IsSecureCmd(command) then
+		-- SetText with a secure command will cause a blocked action error
+		return
+	end
+	--print("AddHistoryLine", text)
+	tinsert(history[frame], text)
+	while #history[frame] > frame:GetHistoryLines() do
+		tremove(history[frame], 1)
+	end
+	index[frame] = #history[frame]
+end
+
+local function IncrementHistorySelection(frame, increment)
+	--print("IncrementHistorySelection", increment)
+	local target = index[frame] + increment
+	if target < 1 then
+		target = #history[frame]
+	elseif target > #history[frame] then
+		target = 1
+	end
+	index[frame] = target
+	local text = history[frame][target]
+	frame:SetText(text)
+	frame:SetCursorPosition(strlen(text))
+end
+
+local function OnArrowPressed(self, key)
+	--print("OnArrowPressed", key)
+	if PhanxChat.db.EnableArrows and not AutoCompleteBox:IsShown() then
+		if key == "UP" then
+			return IncrementHistorySelection(self, 1)
+		elseif key == "DOWN" then
+			return IncrementHistorySelection(self, -1)
+		end
+	end
+end
+
+------------------------------------------------------------------------
+
 function PhanxChat:EnableArrows(frame)
 	local editBox = _G[frame:GetName() .. "EditBox"]
 	if editBox then
 		editBox:SetAltArrowKeyMode(not self.db.EnableArrows)
+		if not history[editBox] then
+			hooksecurefunc(editBox, "AddHistoryLine", AddHistoryLine)
+			editBox:HookScript("OnArrowPressed", OnArrowPressed)
+			history[editBox] = {}
+			index[editBox] = 1
+		end
 	end
 end
 
