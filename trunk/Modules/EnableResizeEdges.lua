321464854c165789c6f4ab764ebe7b6e0361e451
Phanx-28751 <Phanx-28751@06352937-199f-4489-bb52-8e4cf25e7443>
2013-12-13 02:52:10 +0000

    - General cleanup

--- trunk/Modules/EnableResizeEdges.lua
+++ trunk/Modules/EnableResizeEdges.lua
@@ -36,81 +36,86 @@ local function StopResizing(self)
 	FCF_SavePositionAndDimensions(frame)
 end
 
+local function CreateResizeEdges(frame)
+	local name = frame:GetName()
+	local _, _, r, g, b, a, _, locked = FCF_GetChatWindowInfo(frame:GetID())
+
+	for _, point in ipairs(anchorPoints) do
+		local edge = CreateFrame("Button", name .. "Resize" .. point, frame)
+		edge:SetSize(20, 20)
+		edge:SetScript("OnMouseDown", StartResizing)
+		edge:SetScript("OnMouseUp", StopResizing)
+		edge.anchorPoint = strupper(point)
+		frame["resize" .. point] = edge
+		LowerFrameLevel(edge)
+
+		edge.tex = edge:CreateTexture(nil, "BACKGROUND")
+		edge.tex:SetTexture([[Interface\ChatFrame\ChatFrameBorder]])
+		edge.tex:SetWidth(20)
+		edge.tex:SetHeight(20)
+		edge.tex:SetAllPoints(edge)
+		edge.tex:SetVertexColor(r, g, b, math.min(a + 0.25, 1))
+	end
+
+	local bg = _G[name .. "Background"]
+
+	frame.resizeTopLeft:SetPoint("TOPLEFT", bg, -2, 2)
+	frame.resizeTopLeft.tex:SetTexCoord(0, 0.25, 0, 0.125)
+
+	frame.resizeTopRight:SetPoint("TOPRIGHT", bg, 2, 2)
+	frame.resizeTopRight.tex:SetTexCoord(0.75, 1, 0, 0.125)
+
+	frame.resizeBottomLeft:SetPoint("BOTTOMLEFT", bg, -2, -3)
+	frame.resizeBottomLeft.tex:SetTexCoord(0, 0.25, 0.7265625, 0.8515625)
+
+	frame.resizeBottomRight:SetPoint("BOTTOMRIGHT", bg, 2, -3)
+	frame.resizeBottomRight.tex:SetTexCoord(0.75, 1, 0.7265625, 0.8515625)
+
+	frame.resizeTop:SetPoint("LEFT", frame.resizeTopLeft, "RIGHT", 0, 0)
+	frame.resizeTop:SetPoint("RIGHT", frame.resizeTopRight, "LEFT", 0, 0)
+	frame.resizeTop.tex:SetTexCoord(0.25, 0.75, 0, 0.125)
+
+	frame.resizeBottom:SetPoint("LEFT", frame.resizeBottomLeft, "RIGHT", 0, 0)
+	frame.resizeBottom:SetPoint("RIGHT", frame.resizeBottomRight, "LEFT", 0, 0)
+	frame.resizeBottom.tex:SetTexCoord(0.25, 0.75, 0.7265625, 0.8515625)
+
+	frame.resizeLeft:SetPoint("TOP", frame.resizeTopLeft, "BOTTOM", 0, 0)
+	frame.resizeLeft:SetPoint("BOTTOM", frame.resizeBottomLeft, "TOP", 0, 0)
+	frame.resizeLeft.tex:SetTexCoord(0, 0.25, 0.125, 0.7265625)
+
+	frame.resizeRight:SetPoint("TOP", frame.resizeTopRight, "BOTTOM", 0, 0)
+	frame.resizeRight:SetPoint("BOTTOM", frame.resizeBottomRight, "TOP", 0, 0)
+	frame.resizeRight.tex:SetTexCoord(0.75, 1, 0.125, 0.7265625)
+end
+
 ------------------------------------------------------------------------
 
 function PhanxChat:EnableResizeEdges(frame)
 	local name = frame:GetName()
 	local _, _, r, g, b, a, _, locked = FCF_GetChatWindowInfo(frame:GetID())
-
-	if not frame.resizeTopLeft then
-		local bg = _G[name .. "Background"]
-
-		for _, point in ipairs(anchorPoints) do
-			local edge = CreateFrame("Button", name .. "Resize" .. point, frame)
-			frame["resize" .. point] = edge
-
-			edge.anchorPoint = point:upper()
-			edge:SetWidth(20)
-			edge:SetHeight(20)
-			edge:SetScript("OnMouseDown", StartResizing)
-			edge:SetScript("OnMouseUp", StopResizing)
-			LowerFrameLevel(edge)
-
-			edge.tex = edge:CreateTexture(nil, "BACKGROUND")
-			edge.tex:SetTexture([[Interface\ChatFrame\ChatFrameBorder]])
-			edge.tex:SetWidth(20)
-			edge.tex:SetHeight(20)
-			edge.tex:SetAllPoints(edge)
-			edge.tex:SetVertexColor(r, g, b, math.min(a + 0.25, 1))
-
-			if locked then edge:Hide() end
-		end
-
-		frame.resizeTopLeft:SetPoint("TOPLEFT", bg, -2, 2)
-		frame.resizeTopLeft.tex:SetTexCoord(0, 0.25, 0, 0.125)
-
-		frame.resizeTopRight:SetPoint("TOPRIGHT", bg, 2, 2)
-		frame.resizeTopRight.tex:SetTexCoord(0.75, 1, 0, 0.125)
-
-		frame.resizeBottomLeft:SetPoint("BOTTOMLEFT", bg, -2, -3)
-		frame.resizeBottomLeft.tex:SetTexCoord(0, 0.25, 0.7265625, 0.8515625)
-
-		frame.resizeBottomRight:SetPoint("BOTTOMRIGHT", bg, 2, -3)
-		frame.resizeBottomRight.tex:SetTexCoord(0.75, 1, 0.7265625, 0.8515625)
-
-		frame.resizeTop:SetPoint("LEFT", frame.resizeTopLeft, "RIGHT", 0, 0)
-		frame.resizeTop:SetPoint("RIGHT", frame.resizeTopRight, "LEFT", 0, 0)
-		frame.resizeTop.tex:SetTexCoord(0.25, 0.75, 0, 0.125)
-
-		frame.resizeBottom:SetPoint("LEFT", frame.resizeBottomLeft, "RIGHT", 0, 0)
-		frame.resizeBottom:SetPoint("RIGHT", frame.resizeBottomRight, "LEFT", 0, 0)
-		frame.resizeBottom.tex:SetTexCoord(0.25, 0.75, 0.7265625, 0.8515625)
-
-		frame.resizeLeft:SetPoint("TOP", frame.resizeTopLeft, "BOTTOM", 0, 0)
-		frame.resizeLeft:SetPoint("BOTTOM", frame.resizeBottomLeft, "TOP", 0, 0)
-		frame.resizeLeft.tex:SetTexCoord(0, 0.25, 0.125, 0.7265625)
-
-		frame.resizeRight:SetPoint("TOP", frame.resizeTopRight, "BOTTOM", 0, 0)
-		frame.resizeRight:SetPoint("BOTTOM", frame.resizeBottomRight, "TOP", 0, 0)
-		frame.resizeRight.tex:SetTexCoord(0.75, 1, 0.125, 0.7265625)
-	end
-
 	local resizeButton = _G[name .. "ResizeButton"]
 
 	if self.db.EnableResizeEdges then
-		for _, point in ipairs(anchorPoints) do
-			frame["resize" .. point]:Show()
-		end
-
 		resizeButton:SetScript("OnShow", resizeButton.Hide)
 		resizeButton:Hide()
-	else
-		for _, point in ipairs(anchorPoints) do
-			frame["resize" .. point]:Hide()
+
+		if not frame.resizeTopLeft then
+			CreateResizeEdges(frame)
 		end
 
+		for i = 1, #anchorPoints do
+			frame["resize" .. anchorPoints[i]]:SetShown(not locked)
+		end
+	else
+		local _, _, _, _, _, _, _, locked = FCF_GetChatWindowInfo(frame:GetID())
 		resizeButton:SetScript("OnShow", nil)
-		resizeButton:Show()
+		resizeButton:SetShown(not locked)
+
+		if frame.resizeTopLeft then
+			for i = 1, #anchorPoints do
+				frame["resize" .. anchorPoints[i]]:Hide()
+			end
+		end
 	end
 end
 
@@ -120,12 +125,15 @@ function PhanxChat.FCF_SetWindowAlpha(frame, a, doNotSave)
 	PhanxChat.hooks.FCF_SetWindowAlpha(frame, a, doNotSave)
 
 	if not frame.resizeTopLeft then
+		if not self.db.EnableResizeEdges then
+			return
+		end
 		PhanxChat:EnableResizeEdges(frame)
 	end
 
 	local _, _, r, g, b = FCF_GetChatWindowInfo(frame:GetID())
-	for _, point in ipairs(anchorPoints) do
-		frame["resize" .. point].tex:SetVertexColor(r, g, b, math.min(a + 0.25, 1))
+	for i = 1, #anchorPoints do
+		frame["resize" .. anchorPoints[i]].tex:SetVertexColor(r, g, b, math.min(a + 0.25, 1))
 	end
 end
 
@@ -133,12 +141,15 @@ function PhanxChat.FCF_SetWindowColor(frame, r, g, b, doNotSave)
 	PhanxChat.hooks.FCF_SetWindowColor(frame, r, g, b, doNotSave)
 
 	if not frame.resizeTopLeft then
+		if not self.db.EnableResizeEdges then
+			return
+		end
 		PhanxChat:EnableResizeEdges(frame)
 	end
 
 	local _, _, _, _, _, a = FCF_GetChatWindowInfo(frame:GetID())
-	for _, point in ipairs(anchorPoints) do
-		frame["resize" .. point].tex:SetVertexColor(r, g, b, math.min(a + 0.25, 1))
+	for i = 1, #anchorPoints do
+		frame["resize" .. anchorPoints[i]].tex:SetVertexColor(r, g, b, math.min(a + 0.25, 1))
 	end
 end
 
@@ -146,17 +157,14 @@ function PhanxChat.SetChatWindowLocked(i, locked, ...)
 	local frame = _G["ChatFrame" .. i]
 
 	if not frame.resizeTopLeft then
+		if not self.db.EnableResizeEdges then
+			return
+		end
 		PhanxChat:EnableResizeEdges(frame)
 	end
 
-	if locked then
-		for _, point in ipairs(anchorPoints) do
-			frame["resize" .. point]:Hide()
-		end
-	else
-		for _, point in ipairs(anchorPoints) do
-			frame["resize" .. point]:Show()
-		end
+	for i = 1, #anchorPoints do
+		frame["resize" .. anchorPoints[i]]:SetShown(not locked)
 	end
 
 	return PhanxChat.hooks.SetChatWindowLocked(i, locked, ...)
