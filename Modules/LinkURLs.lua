fa20b4f70878989845900635d3bdd475e19be599
Phanx <addons@phanx.net>
2014-11-16 00:23:40 -0800

    Post SVN->Git cleanup

--- Modules/LinkURLs.lua
+++ Modules/LinkURLs.lua
@@ -4,13 +4,7 @@
 	Copyright (c) 2006-2014 Phanx <addons@phanx.net>. All rights reserved.
 	http://www.wowinterface.com/downloads/info6323-PhanxChat.html
 	http://www.curse.com/addons/wow/phanxchat
-
-	Please DO NOT upload this addon to other websites, or post modified
-	versions of it. However, you are welcome to include a copy of it
-	WITHOUT CHANGES in compilations posted on Curse and/or WoWInterface.
-	You are also welcome to use any/all of its code in your own addon, as
-	long as you do not use my name or the name of this addon ANYWHERE in
-	your addon, including its name, outside of an optional attribution.
+	https://github.com/Phanx/PhanxChat
 ----------------------------------------------------------------------]]
 
 local URL_STYLE = "|cff33ccff%s|r"
