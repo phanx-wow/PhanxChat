d067fcbaebeaa00dfb424555ba1d5ac01093a739
Phanx <addons@phanx.net>
2018-07-29 03:24:39 -0700

    Remove scroll to bottom button handling

--- Modules/HideButtons.lua
+++ Modules/HideButtons.lua
@@ -14,65 +14,18 @@ local noop = function() end
 
 ------------------------------------------------------------------------
 
-local function BottomButton_OnClick(self, button)
-	PlaySound(SOUNDKIT.IG_CHAT_BOTTOM)
-	local frame = self:GetParent()
-	if frame.ScrollToBottom then
-		frame:ScrollToBottom()
-	else
-		frame:GetParent():ScrollToBottom()
-	end
-	frame.ScrollToBottomButton:Hide()
-end
-
-local function ChatFrame_OnShow(self)
-	if not PhanxChat.db.HideButtons then return end
-	if self:AtBottom() then
-		self.ScrollToBottomButton:Hide()
-	else
-		self.ScrollToBottomButton:Show()
-	end
-end
-
 function PhanxChat:HideButtons(frame)
 	local name = frame:GetName()
 	local buttonFrame = _G[name .. "ButtonFrame"]
-	local bottomButton = frame.ScrollToBottomButton
-	frame:HookScript("OnShow", ChatFrame_OnShow)
 
 	if self.db.HideButtons then
 		buttonFrame.Show = noop
 		buttonFrame:Hide()
-
-		bottomButton:ClearAllPoints()
-		bottomButton:SetParent(frame)
-		bottomButton:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", 0, -4)
-		bottomButton:SetAlpha(0.75)
-		bottomButton:Hide()
-
-		if not self.hooks[bottomButton] then
-			self.hooks[bottomButton] = { }
-		end
-		if not self.hooks[bottomButton].OnClick then
-			self.hooks[bottomButton].OnClick = bottomButton:GetScript("OnClick")
-			bottomButton:SetScript("OnClick", BottomButton_OnClick)
-		end
 	else
 		buttonFrame.Show = nil
 		buttonFrame:Show()
-
-		bottomButton:ClearAllPoints()
-		bottomButton:SetParent(buttonFrame)
-		bottomButton:SetPoint("BOTTOM", buttonFrame, "BOTTOM", 0, -7)
-		bottomButton:SetAlpha(1)
-		bottomButton:Show()
-
-		if self.hooks[bottomButton] and self.hooks[bottomButton].OnClick then
-			bottomButton:SetScript("OnClick", self.hooks[bottomButton].OnClick)
-			self.hooks[bottomButton].OnClick = nil
-		end
-
-		FCF_UpdateButtonSide(frame)
+		-- TODO: remove if nothing is broken without it
+		-- FCF_UpdateButtonSide(frame)
 	end
 end
 
